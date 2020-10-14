c9974e2c25a4f831d0958f1e6f170defffd5066d
Teun Beijers <teunbeijers@acknowledge.nl>
2020-10-14 15:50:56 +0200

    more work

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
@@ -6,6 +6,17 @@ app = FastAPI()
 @app.get('/{build_id}/?')
 async def get_build(
         *,
-        build_id: str,
-):
+        build_id: str = Query(..., description='The ID of the build, for example: 1a2b3c4e'),
+) -> str:
+    """ Returns the base64-encoded build code to import into Path of Building """
     return f'test: {build_id}'
+
+
+@app.post('/create/?')
+async def create_build(
+        *,
+        build: str,
+) -> str:
+    """ """
+    # @TODO: validation of the build string
+    return '12345678'
--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, Text
+import datetime
+
+from sqlalchemy import Column, Integer, String, Text, DateTime
 
 from database import Base
 
@@ -12,3 +14,8 @@ class Build(Base):
 
     # @TODO: Text is limited to 65k chars iirc, I think that's enough right?
     content = Column(Text)
+
+    creation_time = Column(DateTime(), default=datetime.datetime.utcnow)
+    # @TODO: figure out if this is legal to always log, I think it's allowed for spam protection but might need to be in a privacy policy or something?
+    # @TODO: String for IP is super lazy programming
+    creator_ip = Column(String(255))
