71bb90e2d6c461c57480b9a755906fe17b6abbf0
Teun Beijers <teunbeijers@acknowledge.nl>
2020-10-16 14:43:47 +0200

    accept header checking yay

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI, Query, Depends
+from typing import Optional
+
+from fastapi import FastAPI, Query, Depends, HTTPException, Header
 from sqlalchemy.orm import Session
+from starlette.responses import HTMLResponse, RedirectResponse
 
 import crud
 import models
@@ -12,14 +15,31 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-@app.get('/{build_id}')
+@app.get('/', response_class=HTMLResponse, include_in_schema=False)
+def home():
+    return 'You probably want to visit <a href="/docs">/docs</a> or <a href="/redoc">/redoc</a> instead. ' \
+           'Getting a build requires a' \
+           'This should just be some fancy looking page made by someone who actually knows how to design stuff point ' \
+           'people to the Path of Building download'
+
+
+@app.get('/{build_id}', response_model=Build)
 def get_build(
         *,
         db: Session = Depends(get_db),
         build_id: str = Query(..., description='The ID of the build, for example: 1a2b3c4e'),
-) -> Build:
+        accept: Optional[str] = Header(None),
+):
     """ Returns the base64-encoded build code to import into Path of Building """
-    return Build.from_orm(crud.get_build(db, build_id))
+    # @TODO: I think there has to be a better way to check for application/json
+    if 'application/json' not in accept:
+        return RedirectResponse('/')
+
+    db_build = crud.get_build(db, build_id)
+    if db_build:
+        return Build.from_orm(db_build)
+    else:
+        raise HTTPException(status_code=404, detail='Build not found')
 
 
 @app.post('/create')
